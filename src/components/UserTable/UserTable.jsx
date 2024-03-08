ae7b4b631e886a796cbb1d2abc558e70863f274e
Fedor <fedorv1234@gmail.com>
2024-03-08 14:14:50 +0300

    feature(UserTable): add data fetch logic

--- src/components/UserTable/UserTable.jsx
+++ src/components/UserTable/UserTable.jsx
@@ -1,7 +1,58 @@
+import { useState, useEffect } from "react";
 import Pagination from "../Pagination/Pagination";
 import "./userTable.css";
 
+function UserTableRow({ id, username, email, date, rating }) {
+  const date_parts = date.split("T")[0].split("-");
+  const formated_date =
+    date_parts[2] + "." + date_parts[1] + "." + date_parts[0].slice(-2);
+
+  return (
+    <tr className="table__data-table-row">
+      <td className="table__data-table-cell-data table__data-table-cell-data--first">
+        {username}
+      </td>
+      <td className="table__data-table-cell-data">{email}</td>
+      <td className="table__data-table-cell-data">{formated_date}</td>
+      <td className="table__data-table-cell-data">{rating}</td>
+      <td className="table__data-table-cell-data table__data-table-cell-data--button">
+        <img
+          className="table__data-table-row-dlt-img"
+          alt="cancel"
+          src="/img/icons/cancel.svg"
+        />
+      </td>
+    </tr>
+  );
+}
+
 function UserTable() {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchUsersData = async () => {
+      const requestParams = {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      };
+      try {
+        const response = await fetch(
+          "https://5ebbb8e5f2cfeb001697d05c.mockapi.io/users",
+          requestParams
+        );
+        const data = await response.json();
+        setData(data);
+      } catch (err) {
+        if (err instanceof Error) {
+          console.log(err.message);
+        }
+      }
+    };
+    fetchUsersData();
+  }, []);
+
   return (
     <div className="table">
       <h1 className="table__title">Список пользователей</h1>
@@ -46,81 +97,18 @@ function UserTable() {
             </tr>
           </thead>
           <tbody className="table__data-table-block">
-            <tr className="table__data-table-row">
-              <td className="table__data-table-cell-data table__data-table-cell-data--first">
-                Username
-              </td>
-              <td className="table__data-table-cell-data">test@test.ru</td>
-              <td className="table__data-table-cell-data">23.09.19</td>
-              <td className="table__data-table-cell-data">12</td>
-              <td className="table__data-table-cell-data table__data-table-cell-data--button">
-                <img
-                  className="table__data-table-row-dlt-img"
-                  alt="cancel"
-                  src="/img/icons/cancel.svg"
+            {data.map((item) => {
+              return (
+                <UserTableRow
+                key={item.id}
+                  id={item.id}
+                  username={item.username}
+                  email={item.email}
+                  date={item.registration_date}
+                  rating={item.rating}
                 />
-              </td>
-            </tr>
-            <tr className="table__data-table-row">
-              <td className="table__data-table-cell-data table__data-table-cell-data--first">
-                Username
-              </td>
-              <td className="table__data-table-cell-data">test@test.ru</td>
-              <td className="table__data-table-cell-data">23.09.19</td>
-              <td className="table__data-table-cell-data">12</td>
-              <td className="table__data-table-cell-data table__data-table-cell-data--button">
-                <img
-                  className="table__data-table-row-dlt-img"
-                  alt="cancel"
-                  src="/img/icons/cancel.svg"
-                />
-              </td>
-            </tr>
-            <tr className="table__data-table-row">
-              <td className="table__data-table-cell-data table__data-table-cell-data--first">
-                Username
-              </td>
-              <td className="table__data-table-cell-data">test@test.ru</td>
-              <td className="table__data-table-cell-data">23.09.19</td>
-              <td className="table__data-table-cell-data">12</td>
-              <td className="table__data-table-cell-data table__data-table-cell-data--button">
-                <img
-                  className="table__data-table-row-dlt-img"
-                  alt="cancel"
-                  src="/img/icons/cancel.svg"
-                />
-              </td>
-            </tr>
-            <tr className="table__data-table-row">
-              <td className="table__data-table-cell-data table__data-table-cell-data--first">
-                Username
-              </td>
-              <td className="table__data-table-cell-data">test@test.ru</td>
-              <td className="table__data-table-cell-data">23.09.19</td>
-              <td className="table__data-table-cell-data">12</td>
-              <td className="table__data-table-cell-data table__data-table-cell-data--button">
-                <img
-                  className="table__data-table-row-dlt-img"
-                  alt="cancel"
-                  src="/img/icons/cancel.svg"
-                />
-              </td>
-            </tr>
-            <tr className="table__data-table-row">
-              <td className="table__data-table-cell-data table__data-table-cell-data--first">
-                Username
-              </td>
-              <td className="table__data-table-cell-data">test@test.ru</td>
-              <td className="table__data-table-cell-data">23.09.19</td>
-              <td className="table__data-table-cell-data">12</td>
-              <td className="table__data-table-cell-data table__data-table-cell-data--button">
-                <img
-                  className="table__data-table-row-dlt-img"
-                  alt="cancel"
-                  src="/img/icons/cancel.svg"
-                />
-              </td>
-            </tr>
+              );
+            })}
           </tbody>
         </table>
         <Pagination />
